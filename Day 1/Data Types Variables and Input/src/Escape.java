13bfb73d6d0ca995708dc9eee3c6150a70dc7e3c
Shivam Agrahari <135615585+shivamagrhri@users.noreply.github.com>
2024-02-29 01:08:12 +0530

    Add files via upload
    Java Coding

---
+++ Day 1/Data Types Variables and Input/src/Escape.java
@@ -0,0 +1,8 @@
+public class Escape {
+    public static void main(String[] args) {
+//        System.out.println("Hello \\Shivam Agrahari\\");
+//        System.out.println("Hello \nShivam Agrahari");
+        System.out.println("Hello\bShivam");
+    }
+
+}
---
+++ Day 1/Data Types Variables and Input/src/Variables.java
@@ -0,0 +1,23 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myNumber = 865;
+        System.out.println(myNumber);
+        int yourNumber;
+        yourNumber = 45;
+        System.out.println(yourNumber);
+
+        boolean isVegetarian = true;
+        System.out.println(isVegetarian);
+
+        float myFloat = 5.0f;
+        System.out.println(myFloat);
+
+        double myDouble = 6.0;
+        System.out.println(myDouble);
+
+        String myWish = "Good Morning";
+        System.out.println(myWish);
+
+
+    }
+}
